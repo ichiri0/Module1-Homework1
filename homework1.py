d5e3f135db5f7e05a9aabb841a5608fed0340474
Ichiri0 <ivankochettov@gmail.com>
2024-06-06 19:52:16 +0300

    Исправил 2 пункт, добавил преобразование set в list

---
+++ homework1.py
@@ -0,0 +1,65 @@
+grades = [
+    [5, 3, 3, 5, 4],
+    [2, 2, 2, 3],
+    [4, 5, 5, 2],
+    [4, 4, 3],
+    [5, 5, 5, 4, 5],
+    [2, 2, 2, 2],
+
+]
+
+students = {
+    'Johnny',
+    'Bilbo',
+    'Steve',
+    'Khendrik',
+    'Aaron',
+    'Test'
+}
+
+list_ = sorted(list(students))
+
+print(sorted(list_))
+
+
+average_grade = []
+
+for val in grades:
+    print(sum(val)/len(val))
+
+print(average_grade)
+
+
+dict_ = dict(zip(list_, average_grade))
+
+print(dict_)
+
+
+
+users = [
+    {
+        "name": "Carl",
+        "age": 18
+    },
+    {
+        "name": "John",
+        "age": 20
+    },
+    {
+        "name": "Vano",
+        "age": 21
+    },
+    {
+        "name": "Petr",
+        "age": 22
+    },
+    {
+        "name": "Alex",
+        "age": 44
+    },
+
+]
+
+
+for user in users:
+    print(user)
